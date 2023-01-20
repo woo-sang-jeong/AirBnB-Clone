eb29d983de7f8f8ef384266fa885fe734132f5dc
woo-sang-jeong <jeongwoosangdev@gmail.com>
2023-01-21 04:07:57 +0900

    Authentication Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from . import models
+from users.models import User
 
 
 class TestAmenities(APITestCase):
@@ -101,7 +102,30 @@ class TestAmenity(APITestCase):
     def test_put_amenity(self):
         pass
 
+    # post 방식과 비슷하다
+
     def test_delete_amenity(self):
 
         response = self.client.delete("/api/v1/rooms/amenities/1")
         self.assertEqual(response.status_code, 204)
+
+
+# Authentication Test
+class TestRooms(APITestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="test",
+        )
+        user.set_password("123")
+        user.save()
+        self.user = user
+
+    def test_create_room(self):
+
+        response = self.client.post("/api/v1/rooms/")
+
+        self.assertEqual(response.status_code, 403)
+
+        self.client.force_login(
+            self.user,
+        )
