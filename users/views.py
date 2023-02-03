f3bc2d8199be558b19c23228954506bcdc51ab54
woo-sang-jeong <jeongwoosangdev@gmail.com>
2023-02-03 17:23:30 +0900

    Access Token

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 import jwt
+import requests
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import Response
@@ -131,5 +132,16 @@ class JWTLogIn(APIView):
 class GithubLogIn(APIView):
     def post(self, request):
         code = request.data.get("code")
-        print(code)
-        return Response()
+        access_token = requests.post(
+            f"https://github.com/login/oauth/access_token?code={code}&client_id=68af966b8c528f4fc020&client_secret={settings.GH_SECRET}",
+            headers={"Accept": "application/json"},
+        )
+        access_token = access_token.json().get("access_token")
+        user_data = requests.get(
+            "https://api.github.com/user",
+            headers={
+                "Authorization": f"Bearer {access_token}",
+                "Accept": "application/json",
+            },
+        )
+        print(user_data.json())
